fef48a31fb57823bcb78d77b7639837ffa6cbcb3
Lin Wanqi <a52hertz_2021@qq.com>
2026-03-16 20:52:37 +0800

    init：前端flutter初始化，后端springboot初始化

---
+++ services/api/src/main/java/com/shiji/api/ShijiApiApplication.java
@@ -0,0 +1,13 @@
+package com.shiji.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShijiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShijiApiApplication.class, args);
+	}
+
+}
---
+++ services/api/src/test/java/com/shiji/api/ShijiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiji.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShijiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
